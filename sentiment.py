2e2c1aa6f9248aae73b842136c536c8f02883788
Taylor Bontrager <ttbontra@gmail.com>
2023-08-15 11:08:45 -0400

    changes layout of bullet chart

--- sentiment.py
+++ sentiment.py
@@ -3,7 +3,7 @@ from textblob import TextBlob
 import pandas as pd
 import numpy as np
 
-#data = pd.read_csv('TSLA_History.csv')
+data = pd.read_csv('TSLA_History.csv')
 
 def analyze_stock_sentiment(data):
     data['Momentum'] = data['Close'] - data['Close'].shift(5)
@@ -14,11 +14,19 @@ def analyze_stock_sentiment(data):
     return data['SentimentScore'].iloc[-1]
 
 def create_bullet_graph(sentiment_score):
-    ranges = [-1, -0.5, 0.5, 1]
-    range_colors = ['red', 'yellow', 'green']
+    ranges = [-1, -0.5, 0, 0.5, 1]
+    range_colors = ['red', 'orange', 'yellow', 'green']
 
     # Create bullet graph
     fig = go.Figure()
+
+    fig.add_trace(go.Bar(
+        x=[sentiment_score],
+        y=[0.5],  # Center the bar within the range
+        orientation='h',
+        marker=dict(color='black', line=dict(width=3, color='black')),
+        width=0.25  # Adjust the width of the bar to make it thinner
+    ))
 
     for i in range(len(ranges) - 1):
         fig.add_shape(
@@ -26,18 +34,12 @@ def create_bullet_graph(sentiment_score):
             x0=ranges[i],
             x1=ranges[i+1],
             y0=0,
-            y1=2,
+            y1=1,  # Adjust the height of the colored ranges
             fillcolor=range_colors[i],
-            line=dict(width=0)
+            line=dict(width=0),
+            layer="below"
         )
 
-    #score as a bar
-    fig.add_trace(go.Bar(
-        x=[sentiment_score],
-        y=['Sentiment'],
-        orientation='h',
-        marker=dict(color='black', line=dict(width=3, color='black')),
-    ))
     fig.update_layout(
         title="Stock Sentiment",
         xaxis=dict(range=[-1, 1], title="Sentiment Score"),
@@ -46,7 +48,8 @@ def create_bullet_graph(sentiment_score):
         width=500,
         height=200
     )
-
     return fig
 
-
+sentiment_score = analyze_stock_sentiment(data)  # Assuming 'data' is your stock data DataFrame
+fig = create_bullet_graph(sentiment_score)
+fig.show()
