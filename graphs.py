9cf7c5a4827c3a60cfa922c93ea1698eb080e7ab
Taylor Bontrager <ttbontra@gmail.com>
2023-08-15 15:13:45 -0400

    seperated graphs into own file

---
+++ graphs.py
@@ -0,0 +1,60 @@
+import plotly.graph_objects as go
+import plotly.express as px
+from plotly.subplots import make_subplots
+
+def generate_candlestick(data):
+    fig_candlestick = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.02)
+    fig_candlestick.add_trace(
+        go.Candlestick(
+            x=data.index,
+            open=data['Open'],
+            high=data['High'],
+            low=data['Low'],
+            close=data['Close'],
+            name='OHLC'
+        ),
+        row=1, col=1
+    )
+    fig_candlestick.add_trace(
+        go.Scatter(
+            x=data.index,
+            y=data['Volume'],
+            name='Volume'
+        ),
+        row=2, col=1
+    )
+    fig_candlestick.update_layout(height=500, title_text="Candlestick Chart with Rangeslider")
+    return fig_candlestick
+
+def generate_daily_return(data):
+    fig_daily_return = px.line(data, x=data.index, y='Daily Return', title='Daily Return', markers=True, line_shape='linear')
+    return fig_daily_return
+
+def generate_histogram(data):
+    fig_histogram = px.histogram(data, x='Daily Return', nbins=100, color_discrete_sequence=['green'])
+    return fig_histogram
+
+def generate_box_plots(data):
+    fig_box_plots = make_subplots(rows=1, cols=2, shared_xaxes=True, vertical_spacing=0.02)
+    fig_box_plots.add_trace(go.Box(y=data["Open"], name="Open Box Plot", boxmean=True), row=1, col=1)
+    fig_box_plots.add_trace(go.Box(y=data["High"], name="High Box Plot", boxmean=True), row=1, col=2)
+    fig_box_plots.update_layout(height=500, title_text="Box Plots")
+    return fig_box_plots
+
+def generate_close_distribution(data):
+    fig_dataX_close_hist = px.histogram(data, x="Close", title="Close Distribution")
+    return fig_dataX_close_hist
+
+def generate_open_distribution(data):
+    fig_dataY_close_hist = px.histogram(data, x="Open", title="Open Distribution")
+    return fig_dataY_close_hist
+
+def generate_heatmap_during_covid(data):
+    fig_dataX_heatmap = go.Figure(data=go.Heatmap(z=data.corr(), x=data.columns, y=data.columns, colorscale="Reds"))
+    fig_dataX_heatmap.update_layout(title="Heatmap displaying the relationship between the features of the data (During COVID)")
+    return fig_dataX_heatmap
+
+def generate_heatmap_before_covid(data):
+    fig_dataY_heatmap = go.Figure(data=go.Heatmap(z=data.corr(), x=data.columns, y=data.columns, colorscale="Blues"))
+    fig_dataY_heatmap.update_layout(title="Heatmap displaying the relationship between the features of the data (Before COVID)")
+    return fig_dataY_heatmap
