1b598ba812082d3da2e6a9eb2dc0a8153910b3de
Taylor Bontrager <ttbontra@gmail.com>
2023-10-12 14:51:37 -0400

    update insert function

--- stat_indicators.py
+++ stat_indicators.py
@@ -1,41 +1,87 @@
 import pandas as pd
+import numpy as np
 import mysql.connector
 from ta import add_all_ta_features
-from ta.utils import dropna
+
+# Database configuration
+config = {
+    'user': 'root',
+    'password': 'stable',
+    'host': '127.0.0.1',
+    'database': 'stocks',
+    'raise_on_warnings': True
+}
+ticker = "SPY"
 
 def fetch_stock_data(ticker):
     cnx = mysql.connector.connect(**config)
-    query = f"SELECT date, open_price, close_price, high, low, volume FROM stock_data_optimized WHERE ticker='{ticker}' ORDER BY date"
+    query = f"SELECT date, open_price, high, low, close_price, volume FROM stock_data WHERE ticker='{ticker}' ORDER BY date"
     df = pd.read_sql(query, cnx)
     cnx.close()
+    print(df.head())
     return df
 
 def compute_indicators(df):
-    # Drop NaN values
-    df = dropna(df)
-    
-    # Compute indicators
     df = add_all_ta_features(df, open="open_price", high="high", low="low", close="close_price", volume="volume")
-    
+    print("After adding indicators:\n", df.head())
+
+    # Handle NaN or Infinity values
+    df.replace([np.inf, -np.inf], np.nan, inplace=True)
+    print("After replacing infinite values:\n", df.head())
+
+    df.dropna(subset=['momentum_rsi', 'trend_macd'], inplace=True)
+    df.fillna(0, inplace=True)
+
+    print("After dropping NaN rows:\n", df.head())
+
     return df
 
-def save_indicators_to_db(ticker, df):
+
+def insert_indicators_to_db(ticker, df):
     cnx = mysql.connector.connect(**config)
     cursor = cnx.cursor()
 
     for index, row in df.iterrows():
+        date = row['date']
+
+        # Extract indicators
+        rsi = row['momentum_rsi']
+        macd = row['trend_macd']
+        bollinger_upper = row['volatility_bbm']
+        bollinger_lower = row['volatility_bbl']
+        ppo = row['momentum_ppo']
+        stochastic_oscillator = row['momentum_stoch']
+        roc = row['momentum_roc']
+
         insert_query = """
-        INSERT INTO stock_indicators (ticker, date, rsi, macd, bollinger_upper, bollinger_lower, ppo, stochastic_oscillator, roc, z_score)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            INSERT INTO stock_indicators (ticker, date, rsi, macd, bollinger_upper, bollinger_lower, ppo, stochastic_oscillator, roc)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+            ON DUPLICATE KEY UPDATE 
+            rsi=%s, 
+            macd=%s, 
+            bollinger_upper=%s, 
+            bollinger_lower=%s, 
+            ppo=%s, 
+            stochastic_oscillator=%s, 
+            roc=%s
         """
-        cursor.execute(insert_query, (ticker, row['date'], row['momentum_rsi'], row['trend_macd'], row['volatility_bbm'], row['volatility_bbl'], row['momentum_ppo'], row['momentum_stoch'], row['momentum_roc'], row['close_price_zscore']))
-        
+        data_to_insert = (ticker, date, rsi, macd, bollinger_upper, bollinger_lower, ppo, stochastic_oscillator, roc, rsi, macd, bollinger_upper, bollinger_lower, ppo, stochastic_oscillator, roc)
+        print("Processing:", data_to_insert)
+
+        try:
+            cursor.execute(insert_query, data_to_insert)
+            #print(data_to_insert)
+        except mysql.connector.Error as err:
+            print(f"Error inserting row for {ticker} on date {date}: {err}")
+            print(f"Query: {insert_query}")
+            print(f"Data: {data_to_insert}")
+
     cnx.commit()
     cursor.close()
     cnx.close()
 
 # Example of fetching data for a ticker, computing indicators, and saving them
-ticker = "AAPL"
+#ticker = "AAPL"
 df = fetch_stock_data(ticker)
-df = compute_indicators(df)
-save_indicators_to_db(ticker, df)
+df_indicators = compute_indicators(df)
+insert_indicators_to_db(ticker, df_indicators)
